ba48e78864089f399338c37d67727f4289979a09
Ukeme Bassey <basseyukemme@gmail.com>
2026-04-29 23:19:18 +0100

    edited delete button

--- components/habits/HabitList.tsx
+++ components/habits/HabitList.tsx
@@ -139,7 +139,7 @@ export function HabitList() {
             </DialogDescription>
           </DialogHeader>
 
-          <DialogFooter className="gap-2 sm:gap-0">
+          <DialogFooter className="gap-2">
             <Button
               type="button"
               variant="outline"
